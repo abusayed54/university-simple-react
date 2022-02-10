4c0fe0a06d75de6e605e94ad24829b261a35d4ce
abusayed54 <abusayed444460@gmail.com>
2022-02-10 21:38:18 +0600

    second

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -4,7 +4,12 @@ import './Result.css'
 const Result = () => {
     return (
         <div className='result-container'>
-            <h4>Result</h4>
+            <h3>Result</h3>
+            <div className="result-content">
+                <p className='quantity'><small>Number of University <br /> <span>1</span> </small> </p>
+                <p className='std'>Total Student <br /> <span>#2196f3</span></p>
+                <p className='std'>Total Semister Fee <br /><span>#2196f3</span></p>
+            </div>
         </div>
     );
 };
